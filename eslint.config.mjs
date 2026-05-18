cbc346bc7052742493bc95ac8ac3e6002df84b39
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-05-18 10:22:19 +0500

    feat(hack): i have start the hack

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**", "build/**"],
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+
+      semi: ["error", "always"],
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
